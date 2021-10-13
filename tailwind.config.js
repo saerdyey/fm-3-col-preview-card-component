6bf8bf2d6b004c0596cdbcdf1c6f57ce23ae3a00
Jay Anton Roblico <jayantonroblico23@gmail.com>
2021-10-13 11:57:30 +0800

    Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bright-orange": "hsl(31, 77%, 52%)",
+        "dark-cyan": "hsl(184, 100%, 22%)",
+        "darker-cyan": "hsl(179, 100%, 13%)",
+        "transparent-white": "hsla(0, 0%, 100%, 0.75)",
+        "light-gray": "hsl(0, 0%, 95%)",
+      },
+      fontFamily: {
+        lexend: ["Lexend Deca", "sans-serif"],
+        "big-shoulder": ["Big Shoulders Display", "cursive"],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
